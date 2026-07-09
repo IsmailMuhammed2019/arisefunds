770072db735f9c006533739388954a308f099c86
ismailmuhammed2019 <ismailmuhammed2019@gmail.com>
2026-07-09 15:55:14 +0000

    refactor: remove redundant How It Works call-to-action button from Hero component

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -172,12 +172,6 @@ export default function Hero() {
                       See How it Works
                     </a>
                   </MagneticWrapper>
-                  <a
-                    href="/how-it-works"
-                    className="inline-flex items-center justify-center rounded-full border border-white/25 px-8 py-4 text-base font-semibold text-white hover:border-cyan-400/60 hover:text-cyan-300 transition cursor-pointer"
-                  >
-                    How It Works &rarr;
-                  </a>
                 </div>
 
               </div>
